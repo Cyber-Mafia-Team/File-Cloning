fee521c168983e71b7b4db9e3e223036dc22b294
Cyber-Mafia-Team <143944091+Cyber-Mafia-Team@users.noreply.github.com>
2023-11-02 04:29:21 +0600

    Update MAFIA_RANDOM.py

--- MAFIA_RANDOM.py
+++ MAFIA_RANDOM.py
@@ -1,5 +1,2 @@
-import os,platform
-os.system('git pull')
-Ruhull=platform.architecture()[0]
-if Ruhull=="32bit":__import__("RUHUL32")
-elif Ruhull=="64bit":__import__("RUHUL64")
+import RUHUL64
+RUHUL64.Subscraption()
